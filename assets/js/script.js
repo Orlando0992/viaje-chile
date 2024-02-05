22087cd69ee5291ce4dc02c6cd76343501230e7d
Orlando0992 <131216943+Orlando0992@users.noreply.github.com>
2024-02-04 23:06:22 -0300

    Primer commit
    Subida de archivos

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//Intervalo de imagenes carrusel
+$('#carouselExample').carousel({
+        interval: 3000
+});
+
+//Se crea evento para mostrar msj de correo enviado
+$('#enviar_correo').click(function () {
+    alert("El correo fue enviado correctamente")
+})
